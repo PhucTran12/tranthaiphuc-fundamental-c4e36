240bcb268447d054167351d51ed345bc3683031f
unknown <phucvipkahp@gmail.com>
2019-06-24 01:39:08 +0700

    initial commit

---
+++ Session01/homework/index.js
@@ -0,0 +1,16 @@
+// // + // // BT1
+// // Ta dùng toán tử typeof để phân biệt loại dữ liệu
+// // Các trường hợp tên biến sai: tên bắt đầu bằng chữ số, tên bắt đầu bằng kí hiệu của JS, tên chứa kí tự đặc biệt
+// // vd: 123abc, var, *file
+
+//  + // // BT2
+
+
+// let x = Number(prompt("Enter radius"))
+// a = x * x * 3.14
+// console.log("area =" + a)
+
+// + // // BT3
+let x = Number(prompt("Enter the degree in Celcius"))
+a = x * 1.8 + 32
+console.log(x + "(C) = " + a + "(F)")
